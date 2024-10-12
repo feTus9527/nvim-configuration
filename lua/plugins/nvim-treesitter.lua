fe719157e84ecbe4ad4c9e80191d3ff7bffa5ec7
fetus <snow1013895225@gmail.com>
2024-10-12 18:21:27 +0800

    feat: add treesitter

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+      "windwp/nvim-ts-autotag",
+      "axelvc/template-string.nvim"
+    },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "lua",
+          "vim"
+        }
+      })
+    end
+  }
+}
