e66342003826e7e15b2ba8ade1a62ea60a1c31bf
fetus <snow1013895225@gmail.com>
2024-10-12 18:07:23 +0800

    init: basic structure of the code

---
+++ lua/options.lua
@@ -0,0 +1,11 @@
+local _opt = vim.opt
+
+_opt.number = true
+_opt.relativenumber = true
+_opt.tabstop = 2
+_opt.shiftwidth = 2
+_opt.expandtab = true
+_opt.autoindent = true
+
+_opt.ignorecase = true
+_opt.smartcase = true
