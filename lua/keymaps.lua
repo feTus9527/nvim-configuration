6e15bfb0aa13188eb3471d176fb1a7b57d624328
fetus <snow1013895225@gmail.com>
2024-10-13 14:33:58 +0800

    feat: improve keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,3 +2,17 @@ vim.g.mapleader = " "
 
 local _keymap = vim.keymap
 
+local opt = {
+  noremap = true,
+  expr = true,
+}
+
+_keymap.set("n", "<c-a>", "ggVG")             -- select all
+_keymap.set({ "n", "x" }, "<leader>p", "0p")  -- only copy manually copied text, not deleted text
+
+_keymap.set("n", "<leader>q", "<cmd>q<cr>")   -- :q
+_keymap.set("n", "<leader>w", "<cmd>w<cr>")   -- :w
+_keymap.set("n", "<leader>x", "<cmd>x<cr>")   -- :x
+
+_keymap.set("n", "j", [[v:count ? "j" : "gj"]], opt)
+_keymap.set("n", "k", [[v:count ? "k" : "gk"]], opt)
