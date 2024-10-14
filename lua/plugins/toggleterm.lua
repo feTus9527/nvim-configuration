ff41720912bcac77e9097f83311bd95c26ee6778
fetus <snow1013895225@gmail.com>
2024-10-14 16:40:37 +0800

    feat: add intergrated terminal

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    config = function()
+      require("toggleterm").setup({
+        open_mapping = [[<c-\>]],
+        direction = "float",
+        float_opts = {
+          border = "curved",
+          width = 130,
+          height = 30,
+        },
+      })
+    end,
+  },
+}
