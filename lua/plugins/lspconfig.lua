c240f719efd77bc256b943ea85db61c9f163a5bb
fetus <snow1013895225@gmail.com>
2024-10-14 16:39:41 +0800

    feat: edit lspconfig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -4,6 +4,13 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
 
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+      capabilities.textDocument.foldingRange = {
+        dynamicRegistration = false,
+        lineFoldingOnly = true,
+      }
+
       -- lua lspconfig
       lspconfig.lua_ls.setup({
         settings = {
