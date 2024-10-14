7dd8d668080722d0b1895bfa9d5eedccef5ca5b0
fetus <snow1013895225@gmail.com>
2024-10-14 09:55:19 +0800

    feat: telescope settings

--- lua/cfg-telescope.lua
+++ lua/cfg-telescope.lua
@@ -1,6 +1,33 @@
 local builtin = require("telescope.builtin")
 
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>f", builtin.find_files, {})
+vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
+
+local actions = require("telescope.actions")
+require("telescope").setup({
+  defaults = {
+    mappings = {
+      i = {
+        ["esc"] = actions.close
+      }
+    },
+    file_ignore_patterns = {
+      "^.git/",
+      "lazy-lock.json",
+      "node_modules",
+      "yarn.lock",
+      "pnpm-lock.yaml",
+      "package-lock.json"
+    },
+    dynamic_preview_title = true,
+    path_display = { "smart" }
+  },
+  pickers = {
+    find_files = {
+      hidden = true
+    }
+  },
+  layout_config = {
+    horizontal = { preview_cutoff = 100, preview_width = .5 }
+  }
+})
