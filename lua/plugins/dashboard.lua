f8edba57db145a02c33db1a01d9f99240b216bcd
fetus <snow1013895225@gmail.com>
2024-10-15 10:34:46 +0800

    feat: add banner

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -4,7 +4,27 @@ return {
     event = "VimEnter",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("dashboard").setup({})
+      local p = vim.fn.getcwd()
+      local banner_path = p .. "/AppData/Local/nvim/banner.txt"
+
+      local banner = {}
+      local file = io.open(banner_path, "r")
+      if file then
+        for line in file:lines() do
+          table.insert(banner, line)
+        end
+        file:close()
+        print("Banner file read successfully")
+      else
+        print("Cannot open banner file " .. banner_path)
+        banner = { "Default header", "when file cannot be read" }
+      end
+      require("dashboard").setup({
+        config = {
+          header = banner,
+        },
+      })
     end,
   },
 }
+
